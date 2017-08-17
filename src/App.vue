6d04aa2ba50877b03d46251de53ebe6ab4f8f4d5
Jin Ke <jinkebj@126.com>
2017-08-17 23:13:51 +0800

    add FAB on UI

--- src/App.vue
+++ src/App.vue
@@ -20,22 +20,23 @@
       </md-toolbar>
     </md-whiteframe>
 
-    <!-- <md-speed-dial md-open="click" md-direction="top" class="md-fab-bottom-center">
-      <md-button class="md-fab" md-fab-trigger>
-        <md-icon md-icon-morph>close</md-icon>
-        <md-icon>add</md-icon>
-      </md-button>
 
-      <md-button class="md-fab md-primary md-mini md-clean">
-        <md-icon>note_add</md-icon>
-      </md-button>
-
-      <md-button class="md-fab md-primary md-mini md-clean">
-        <md-icon>alarm_add</md-icon>
-      </md-button>
-    </md-speed-dial> -->
 
     <md-list class="page-content md-triple-line">
+      <md-speed-dial md-open="click" md-direction="top" class="md-fab-bottom-right">
+        <md-button class="md-fab" md-fab-trigger>
+          <md-icon md-icon-morph>close</md-icon>
+          <md-icon>add</md-icon>
+        </md-button>
+
+        <md-button class="md-fab md-primary md-mini md-clean">
+          <md-icon>note_add</md-icon>
+        </md-button>
+
+        <md-button class="md-fab md-primary md-mini md-clean">
+          <md-icon>alarm_add</md-icon>
+        </md-button>
+      </md-speed-dial>
       <md-list-item>
         <md-avatar class="md-avatar-icon">
           <md-icon>folder</md-icon>
