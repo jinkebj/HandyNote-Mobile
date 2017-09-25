be6982e5bff5b4474008d68005a95f5a0673135d
Jin Ke <jinkebj@126.com>
2017-09-25 23:51:27 +0800

    update note folder UI to utilize title slot

--- src/components/NoteFolder.vue
+++ src/components/NoteFolder.vue
@@ -1,12 +1,14 @@
 <template>
   <mu-list class="note-folder-container">
-    <mu-list-item title="My Folders (12 / 30)" titleClass="note-top-folder">
+    <mu-list-item>
       <mu-icon slot="left" value="folder" />
+      <div slot="title" class="note-top-folder" @click="$router.replace('/recents')">My Folders (12 / 30)</div>
       <mu-icon-menu slot="after" icon="more_vert">
         <mu-menu-item title="New Sub Folder" />
       </mu-icon-menu>
 
-      <mu-list-item slot="nested" title="Added (10 / 23)" titleClass="note-folder-title">
+      <mu-list-item slot="nested">
+        <div slot="title" class="note-folder-title" @click="$router.replace('/recents')">Added (10 / 23)</div>
         <mu-icon-menu slot="after" icon="more_vert">
           <mu-menu-item title="New Sub Folder" />
           <mu-menu-item title="Rename" />
@@ -14,7 +16,8 @@
           <mu-menu-item title="Delete" />
         </mu-icon-menu>
 
-        <mu-list-item title="Drafts (5)" slot="nested" titleClass="note-folder-title">
+        <mu-list-item slot="nested">
+          <div slot="title" class="note-folder-title" @click="$router.replace('/recents')">Drafts (5)</div>
           <mu-icon-menu slot="after" icon="more_vert">
             <mu-menu-item title="New Sub Folder" />
             <mu-menu-item title="Rename" />
@@ -23,7 +26,8 @@
           </mu-icon-menu>
         </mu-list-item>
 
-        <mu-list-item title="Java (8)" slot="nested" titleClass="note-folder-title">
+        <mu-list-item slot="nested">
+          <div slot="title" class="note-folder-title" @click="$router.replace('/recents')">Java (8)</div>
           <mu-icon-menu slot="after" icon="more_vert">
             <mu-menu-item title="New Sub Folder" />
             <mu-menu-item title="Rename" />
@@ -33,7 +37,8 @@
         </mu-list-item>
       </mu-list-item>
 
-      <mu-list-item slot="nested" title="Another (10 / 23)" titleClass="note-folder-title">
+      <mu-list-item slot="nested">
+        <div slot="title" class="note-folder-title" @click="$router.replace('/recents')">Another (10 / 23)</div>
         <mu-icon-menu slot="after" icon="more_vert">
           <mu-menu-item title="New Sub Folder" />
           <mu-menu-item title="Rename" />
@@ -41,7 +46,8 @@
           <mu-menu-item title="Delete" />
         </mu-icon-menu>
 
-        <mu-list-item title="Drafts (5)" slot="nested" titleClass="note-folder-title">
+        <mu-list-item slot="nested">
+          <div slot="title" class="note-folder-title" @click="$router.replace('/recents')">Drafts (5)</div>
           <mu-icon-menu slot="after" icon="more_vert">
             <mu-menu-item title="New Sub Folder" />
             <mu-menu-item title="Rename" />
@@ -50,7 +56,8 @@
           </mu-icon-menu>
         </mu-list-item>
 
-        <mu-list-item title="Java (8)" slot="nested" titleClass="note-folder-title">
+        <mu-list-item slot="nested">
+          <div slot="title" class="note-folder-title" @click="$router.replace('/recents')">Java (8)</div>
           <mu-icon-menu slot="after" icon="more_vert">
             <mu-menu-item title="New Sub Folder" />
             <mu-menu-item title="Rename" />
@@ -60,14 +67,16 @@
         </mu-list-item>
       </mu-list-item>
 
-      <mu-list-item slot="nested" title="Updated (2 / 6)" titleClass="note-folder-title">
+      <mu-list-item slot="nested">
+        <div slot="title" class="note-folder-title" @click="$router.replace('/recents')">Updated (2 / 6)</div>
         <mu-icon-menu slot="after" icon="more_vert">
           <mu-menu-item title="New Sub Folder" />
           <mu-menu-item title="Rename" />
           <mu-menu-item title="Move To" />
           <mu-menu-item title="Delete" />
         </mu-icon-menu>
-        <mu-list-item title="Drafts (4)" slot="nested" titleClass="note-folder-title">
+        <mu-list-item slot="nested">
+          <div slot="title" class="note-folder-title" @click="$router.replace('/recents')">Drafts (4)</div>
           <mu-icon-menu slot="after" icon="more_vert">
             <mu-menu-item title="New Sub Folder" />
             <mu-menu-item title="Rename" />
@@ -78,8 +87,9 @@
       </mu-list-item>
     </mu-list-item>
 
-    <mu-list-item title="Trash (5)" titleClass="note-top-folder">
+    <mu-list-item>
       <mu-icon slot="left" value="delete"/>
+      <div slot="title" class="note-folder-title" @click="$router.replace('/recents')">Trash (5)</div>
       <mu-icon-menu slot="after" icon="more_vert">
         <mu-menu-item title="Empty Trash" />
         <mu-menu-item title="Restore All" />
