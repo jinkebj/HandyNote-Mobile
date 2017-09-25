7b20f01be7c3144d84fbf0b62f75d1742e3b33fd
Jin Ke <jinkebj@126.com>
2017-09-25 16:38:20 +0800

    support pull to refresh on note list UI

--- src/components/NoteList.vue
+++ src/components/NoteList.vue
@@ -1,18 +1,21 @@
 <template>
-  <mu-list>
-    <mu-list-item class="list-item-divider" v-for="listItem in listItems" :key="listItem._id">
-      <div>
-        <div class="list-item-name">{{listItem.name | truncate(35)}}</div>
-        <div class="list-item-digest">{{listItem.digest | truncate(70)}}</div>
-        <div class="list-item-info">{{listItem.updated_at | fmtDate}} {{listItem.folder_name}}</div>
-      </div>
+  <div>
+    <mu-refresh-control :refreshing="refreshing" :trigger="trigger" @refresh="refresh"/>
+    <mu-list>
+      <mu-list-item class="list-item-divider" v-for="listItem in listItems" :key="listItem._id">
+        <div>
+          <div class="list-item-name">{{listItem.name | truncate(35)}}</div>
+          <div class="list-item-digest">{{listItem.digest | truncate(70)}}</div>
+          <div class="list-item-info">{{listItem.updated_at | fmtDate}} {{listItem.folder_name}}</div>
+        </div>
 
-      <mu-icon-menu slot="right" icon="more_vert">
-        <mu-menu-item title="Move To" />
-        <mu-menu-item title="Delete" />
-      </mu-icon-menu>
-    </mu-list-item>
-  </mu-list>
+        <mu-icon-menu slot="right" icon="more_vert">
+          <mu-menu-item title="Move To" />
+          <mu-menu-item title="Delete" />
+        </mu-icon-menu>
+      </mu-list-item>
+    </mu-list>
+  </div>
 </template>
 
 <style scoped>
@@ -44,12 +47,15 @@ import Model from '@/models'
 export default {
   data () {
     return {
+      refreshing: false,
+      trigger: null,
       listItems: []
     }
   },
 
   mounted () {
     this.loadNoteList()
+    this.trigger = this.$el
   },
 
   methods: {
@@ -62,6 +68,14 @@ export default {
         .catch(function (error) {
           console.log(error)
         })
+    },
+
+    refresh () {
+      this.refreshing = true
+      setTimeout(() => {
+        this.loadNoteList()
+        this.refreshing = false
+      }, 1000)
     }
   }
 }
